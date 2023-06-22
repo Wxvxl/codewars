ee5cbbe209ab8695a3b6c2a26780d81661e2cab1
Calvin <metal123cool1115@gmail.com>
2023-06-22 22:07:22 +0800

    Initial commit

---
+++ validbraces.py
@@ -0,0 +1,34 @@
+def validBraces(string):
+
+    open_braces = ["(", "[", "{"]
+    braces_dictionary = {
+        "(" : ")",
+        "{" : "}",
+        "[" : "]"  
+    }
+    
+    open_brace_in_string = []
+    closed_brace_in_string = []
+
+    for i in range(len(string)):
+        if string[i] in open_braces:
+            open_brace_in_string.append(i)
+        else:
+            closed_brace_in_string.append(i)
+    
+    if len(open_brace_in_string) == 0:
+        return False
+    if len(closed_brace_in_string) == 0:
+        return False
+    
+    for i in open_brace_in_string:
+        current_closing_brace = braces_dictionary[string[i]]
+        for y in closed_brace_in_string:
+            if string[y] == current_closing_brace:
+                if y - i == 1:
+                    validity = True
+            else:
+                validity = False
+    return validity
+
+validBraces("([{}])")
