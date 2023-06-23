ebbe9b42000f667c7f1e718e1a9e0eaed730fee6
Calvin <metal123cool1115@gmail.com>
2023-06-23 22:57:06 +0800

    Initial commit.. ugh

---
+++ reversewords.py
@@ -0,0 +1,22 @@
+# https://www.codewars.com/kata/5259b20d6021e9e14c0010d4
+def reverse_words(text):
+    whitespace_location = []
+
+    new_word = ""
+    for i in range(len(text)):
+        if text[i] == " ":
+            whitespace_location.append(i)
+    word_list = text.split()
+    for i in range(len(word_list)):
+        word_list[i] = word_list[i][::-1]
+
+    for word in word_list:
+        new_word += word
+
+    for i in range(len(new_word)+99):
+        for x in whitespace_location:
+            if i == x:
+                new_word = new_word[:i] + " " + new_word[i:]
+                whitespace_location.remove(x)
+
+    return new_word
